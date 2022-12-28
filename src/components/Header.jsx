f9be2f6c514da28eeb84807288a55386b2dc5da0
pratyush-45 <pratyush4599@gmail.com>
2022-12-28 21:43:15 +0530

    Frontend build complete with react

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Highlight } from "@mui/icons-material";
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1><Highlight /> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
